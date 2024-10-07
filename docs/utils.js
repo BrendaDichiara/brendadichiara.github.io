522a40164f8a3d7cd43ab5685c5a6bd95f5cebab
Brenda Dichiara <brendy-15@hotmail.com>
2024-10-06 23:51:44 -0300

    Río 2025

---
+++ docs/utils.js
@@ -0,0 +1,38 @@
+// Get the main content container element
+let content = document.getElementById('content')
+
+// Get the sidebar, navbar and sidebar items containers
+let navbar_items = document.getElementById("navbar-items");
+let sidebar     = document.getElementById("sidebar");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+  if (sidebar.style.display === 'block') {
+    sidebar.style.display = 'none';
+  } else {
+    sidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+  sidebar.style.display = "none";
+}
+
+// load markdown content
+function load_content(id) {
+    fetch(`${id}.html`)
+    .then(response => response.text())
+    .then(text => {
+        content.innerHTML = text
+    })
+    return true
+}
+
+function markdown_to_html(md) {
+    return marked.parse(md)
+}
+
+function init() {
+    load_content('about')
+}
